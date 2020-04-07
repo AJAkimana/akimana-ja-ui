be19617a2ccf5e00910fc3bff3c43887e07387f1
Akimana Linux <akimana@linux.me>
2020-04-07 21:32:53 +0200

    Create header for the profile

---
+++ src/utils/constants.js
@@ -0,0 +1,34 @@
+export const socials = [
+  { name: 'twitter', link: '#' },
+  { name: 'facebook', link: '#' },
+  { name: 'instagram', link: '#' },
+  { name: 'skype', link: '#' },
+  { name: 'linkedin', link: '#' },
+];
+export const navs = [
+  {
+    name: 'About',
+    link: '#about',
+    cssClass: 'user',
+  },
+  {
+    name: 'Resume',
+    link: '#resume',
+    cssClass: 'file-blank',
+  },
+  {
+    name: 'Portfolio',
+    link: '#portfolio',
+    cssClass: 'book-content',
+  },
+  {
+    name: 'Services',
+    link: '#services',
+    cssClass: 'server',
+  },
+  {
+    name: 'Contact',
+    link: '#contact',
+    cssClass: 'envelope',
+  },
+];

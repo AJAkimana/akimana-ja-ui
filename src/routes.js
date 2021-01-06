7a5251b2f42305a3bfefcf0bfc894f68b7b97a45
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-01-06 15:03:54 +0200

    Update my web inforamtion

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
+import { Redirect } from 'react-router-dom';
 import HomePage from './views/homepage';
 
 export const routes = [
-  {
-    path: '/',
-    exact: true,
-    component: HomePage
-  }
+	{
+		path: '/',
+		exact: true,
+		component: HomePage
+	},
+	{
+		component: () => <Redirect to='/' />
+	}
 ];
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,89 +1,89 @@
 export const socials = [
-  { name: 'github', link: 'https://github.com/AJAkimana' },
-  { name: 'twitter', link: 'https://twitter.com/AJAkimana' },
-  {
-    name: 'linkedin',
-    link: 'https://linkedin.com/in/akimana-jean-d-amour-8553a167',
-  },
+	{ name: 'github', link: 'https://github.com/AJAkimana' },
+	{ name: 'twitter', link: 'https://twitter.com/AJAkimana' },
+	{
+		name: 'linkedin',
+		link: 'https://www.linkedin.com/in/jean-d-amour-akimanizanye-8553a167/'
+	}
 ];
 export const navs = [
-  {
-    name: 'About',
-    link: '#about',
-    cssClass: 'user',
-  },
-  {
-    name: 'Skills',
-    link: '#skills',
-    cssClass: 'server',
-  },
-  {
-    name: 'Resume',
-    link: '#resume',
-    cssClass: 'file-blank',
-  },
-  // {
-  //   name: 'Portfolio',
-  //   link: '#portfolio',
-  //   cssClass: 'book-content',
-  // },
-  // {
-  //   name: 'Services',
-  //   link: '#services',
-  //   cssClass: 'server',
-  // },
-  {
-    name: 'Contact',
-    link: '#contact',
-    cssClass: 'envelope',
-  },
+	{
+		name: 'About',
+		link: '#about',
+		cssClass: 'user'
+	},
+	{
+		name: 'Skills',
+		link: '#skills',
+		cssClass: 'server'
+	},
+	{
+		name: 'Resume',
+		link: '#resume',
+		cssClass: 'file-blank'
+	},
+	// {
+	//   name: 'Portfolio',
+	//   link: '#portfolio',
+	//   cssClass: 'book-content',
+	// },
+	// {
+	//   name: 'Services',
+	//   link: '#services',
+	//   cssClass: 'server',
+	// },
+	{
+		name: 'Contact',
+		link: '#contact',
+		cssClass: 'envelope'
+	}
 ];
 export const skills = [
-  {
-    type: 'dbs',
-    subSkills: [
-      {
-        name: 'MySQL',
-        progress: 80,
-      },
-      {
-        name: 'Postgres',
-        progress: 70,
-      },
-      {
-        name: 'Mongo',
-        progress: 80,
-      },
-    ],
-  },
-  {
-    type: 'backends',
-    subSkills: [
-      {
-        name: 'Node/Express',
-        progress: 90,
-      },
-      {
-        name: 'PHP/Lumen',
-        progress: 70,
-      },
-      {
-        name: 'Python/Django',
-        progress: 60,
-      },
-    ],
-  },
-  {
-    type: 'frontends',
-    subSkills: [
-      {
-        name: 'React/Redux',
-        progress: 90,
-      },
-      {
-        name: 'Angular/AngularJS',
-        progress: 50,
-      },
-    ],
-  },
+	{
+		type: 'dbs',
+		subSkills: [
+			{
+				name: 'MySQL',
+				progress: 80
+			},
+			{
+				name: 'Postgres',
+				progress: 70
+			},
+			{
+				name: 'Mongo',
+				progress: 80
+			}
+		]
+	},
+	{
+		type: 'backends',
+		subSkills: [
+			{
+				name: 'Node/Express',
+				progress: 90
+			},
+			{
+				name: 'PHP/Lumen',
+				progress: 70
+			},
+			{
+				name: 'Python/Django',
+				progress: 60
+			}
+		]
+	},
+	{
+		type: 'frontends',
+		subSkills: [
+			{
+				name: 'React/Redux',
+				progress: 90
+			},
+			{
+				name: 'Angular/AngularJS',
+				progress: 50
+			}
+		]
+	}
 ];
